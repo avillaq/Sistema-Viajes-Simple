9682171542de9494d0eb9aaeb695ddc5a518bd62
avillaq <villafuertequispealex@gmail.com>
2024-06-01 18:53:17 -0500

    Creamos un modelo basico Destination y lo instanciamos en la vista index

--- travello/views.py
+++ travello/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Destination
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    dest1 = Destination()
+    dest1.name = 'Mumbai'
+    dest1.desc = 'The City That Never Sleeps'
+    dest1.price = 700
+    dest1.img = 'destination_1.jpg'
+
+
+    return render(request, 'index.html', {'dest1': dest1})
