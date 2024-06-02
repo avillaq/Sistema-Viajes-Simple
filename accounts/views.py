9c3112a8e6670867d7771d00ec817cc3994a7d2a
avillaq <villafuertequispealex@gmail.com>
2024-06-01 21:15:37 -0500

    Se añade un un mensaje a la pagina del formulario cuando el no es posible registrar al usuario

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
+from django.contrib import messages
 
 # Create your views here.
 def register(request):
@@ -13,15 +14,18 @@ def register(request):
 
         if password1 == password2:
             if User.objects.filter(username=username).exists():
-                print('Username taken')
+                messages.info(request, 'Username taken')
+                return redirect('register')
             elif User.objects.filter(email=email).exists():
-                print('Email taken')
+                messages.info(request, 'Email taken')
+                return redirect('register')
             else:
                 user = User.objects.create_user(username=username, password=password1, email=email, first_name=first_name, last_name=last_name)
                 user.save()
                 print('User created')
         else:
             print('Password not matching...')
+            return redirect('register')
         return redirect('/')
     else:
         return render(request, 'register.html')
