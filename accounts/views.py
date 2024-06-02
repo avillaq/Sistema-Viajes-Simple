c3930d725fd04644c9f0b71693b539a412ef32f8
avillaq <villafuertequispealex@gmail.com>
2024-06-01 21:19:09 -0500

    Creamos un template login con un formulario para el inicio de sesion de los usuarios. Ademas, se añade la ruta login en la aplicacion

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,9 @@ from django.contrib.auth.models import User, auth
 from django.contrib import messages
 
 # Create your views here.
+def login(request):
+    pass
+
 def register(request):
     if request.method == 'POST':
         first_name = request.POST['first_name']
@@ -24,7 +27,7 @@ def register(request):
                 user.save()
                 print('User created')
         else:
-            print('Password not matching...')
+            messages.info(request, 'Password not matching')
             return redirect('register')
         return redirect('/')
     else:
